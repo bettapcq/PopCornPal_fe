59a527be526be8cfb259a90937e0740bbc0a2ef4
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-03-08 18:51:32 +0100

    update navbar design active and routing

--- src/components/superiorNavBAr/SuperiorNavBar.jsx
+++ src/components/superiorNavBAr/SuperiorNavBar.jsx
@@ -2,9 +2,9 @@ import "../superiorNavBar/SuperiorNavBar.scss";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Image from "react-bootstrap/Image";
+import { Image } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link } from "react-router-dom";
 import avatar_placeholder from "../../assets/img/avatar_placeholder.jpg";
 import { faBell, faComment } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,7 +15,7 @@ function SuperiorNavBar() {
       <Container className="justify-content-between" fluid>
         {/* logo */}
 
-        <Navbar.Brand md={2} as={Link} to="/home">
+        <Navbar.Brand md={2} as={NavLink} to="/private/home">
           <img
             className="logo-mini"
             src="/src/assets/img/logo_mini.png"
@@ -30,24 +30,24 @@ function SuperiorNavBar() {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto my-3 align-items-center my-collapse">
-            <Nav.Link as={Link} to="/home">
+            <Nav.Link as={NavLink} to="/private/home">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/profile">
-              Profile
+            <Nav.Link as={NavLink} to="/private/profile/me">
+              My Profile
             </Nav.Link>
-            <Nav.Link as={Link} to="/events">
-              Your Events
+            <Nav.Link as={NavLink} to="/private/events">
+              My Events
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
 
         {/* icons */}
 
-        <Nav.Link as={Link} to="/messages" className="px-3">
+        <Nav.Link as={NavLink} to="/private/messages" className="px-3">
           <FontAwesomeIcon icon={faComment} className="nav-icon" />
         </Nav.Link>
-        <Nav.Link as={Link} to="/notifications" className="px-3">
+        <Nav.Link as={NavLink} to="/private/notifications" className="px-3">
           <FontAwesomeIcon icon={faBell} className="nav-icon" />
         </Nav.Link>
 
@@ -55,25 +55,25 @@ function SuperiorNavBar() {
 
         <NavDropdown
           title={
-            <img
-              className="rounded-circle avatar mx-2 "
+            <Image
+              className="rounded-circle avatar"
               src={avatar_placeholder}
               alt="avatar"
-              width={40}
-              height={40}
+              height={45}
+              width={45}
             />
           }
           id="profile-dropdown"
           align="end"
         >
-          <NavDropdown.Item to="/profile" as={Link}>
-            View Profile
+          <NavDropdown.Item as={NavLink} to="/private/profile/me">
+            My Profile
           </NavDropdown.Item>
-          <NavDropdown.Item to="/edit-profile" as={Link}>
+          <NavDropdown.Item as={NavLink} to="/private/edit-profile">
             Settings
           </NavDropdown.Item>
           <NavDropdown.Divider color="$text-light" />
-          <NavDropdown.Item to="/logout" as={Link}>
+          <NavDropdown.Item as={NavLink} to="/">
             Log out
           </NavDropdown.Item>
         </NavDropdown>
--- src/pages/PrivatePages.jsx
+++ src/pages/PrivatePages.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import ProfilePage from "./profilePage/ProfilePage";
+
 import SuperiorNavBar from "../components/superiorNavBAr/SuperiorNavBar";
+import HomePage from "./HomePage/HomePage";
 
 function PrivatePages() {
   return (
     <>
       <SuperiorNavBar />
       <Routes>
-        <Route path="profile/:userId" element={<ProfilePage />} />
-        {/* <Route path="/home" element={<EventsPage />} /> */}
+        {/* <Route path="/profile/:userId" element={<ProfilePage />} /> */}
+        <Route path="/home" element={<HomePage />} />
         {/* <Route path="/event" element={<EventsPage />} /> */}
         {/* <Route path="/settings" element={<SettingsPage />} /> */}
         {/* <Route path="/messages" element={<MessagesPage />} /> */}
