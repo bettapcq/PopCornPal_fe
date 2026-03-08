4131ea7e586807810acf1ebb6b3deeaa7c783fa1
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-03-08 17:37:36 +0100

    add navbar partial design

--- src/components/superiorNavBAr/SuperiorNavBar.jsx
+++ src/components/superiorNavBAr/SuperiorNavBar.jsx
@@ -1,31 +1,82 @@
+import "../superiorNavBar/SuperiorNavBar.scss";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+import avatar_placeholder from "../../assets/img/avatar_placeholder.jpg";
+import { faBell, faComment } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SuperiorNavBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar expand="lg" className="my-nav bg-body-tertiary mt-3 mb-4">
+      <Container className="justify-content-between" fluid>
+        {/* logo */}
+
+        <Navbar.Brand md={2} as={Link} to="/home">
+          <img
+            className="logo-mini"
+            src="/src/assets/img/logo_mini.png"
+            alt="logo"
+          />
+        </Navbar.Brand>
+
+        {/* toggle */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="my-toggle" />
+
+        {/* collapse mobile links */}
+
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="me-auto my-3 align-items-center my-collapse">
+            <Nav.Link as={Link} to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/profile">
+              Profile
+            </Nav.Link>
+            <Nav.Link as={Link} to="/events">
+              Your Events
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
+        {/* icons */}
+
+        <Nav.Link as={Link} to="/messages" className="px-3">
+          <FontAwesomeIcon icon={faComment} className="nav-icon" />
+        </Nav.Link>
+        <Nav.Link as={Link} to="/notifications" className="px-3">
+          <FontAwesomeIcon icon={faBell} className="nav-icon" />
+        </Nav.Link>
+
+        {/* desktop avatar */}
+
+        <NavDropdown
+          title={
+            <img
+              className="rounded-circle avatar mx-2 "
+              src={avatar_placeholder}
+              alt="avatar"
+              width={40}
+              height={40}
+            />
+          }
+          id="profile-dropdown"
+          align="end"
+        >
+          <NavDropdown.Item to="/profile" as={Link}>
+            View Profile
+          </NavDropdown.Item>
+          <NavDropdown.Item to="/edit-profile" as={Link}>
+            Settings
+          </NavDropdown.Item>
+          <NavDropdown.Divider color="$text-light" />
+          <NavDropdown.Item to="/logout" as={Link}>
+            Log out
+          </NavDropdown.Item>
+        </NavDropdown>
       </Container>
     </Navbar>
   );
