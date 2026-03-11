8f4875133a33e6303349e1c06d9315a612d78488
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-03-11 03:41:37 +0100

    started events fetch to implement in components

---
+++ src/redux/actions/EventActions.js
@@ -0,0 +1,81 @@
+export const GET_EVENTS_ERROR = "GET_EVENTS_ERROR";
+export const GET_EVENTS_LOADING = "GET_EVENTS_LOADING";
+export const GET_HOME_EVENTS_SUCCESS = "GET_HOME_EVENTS_SUCCESS";
+export const GET_USERS_PAST_EVENTS_SUCCESS = "GET_USER_PAST_EVENTS_SUCCESS";
+export const GET_USERS_FUTURE_EVENTS_SUCCESS =
+  "GET_USERS_FUTURE_EVENTS_SUCCESS";
+export const GET_USERS_JOINED_EVENTS_SUCCESS =
+  " GET_USERS_JOINED_EVENTS_SUCCESS";
+
+// --------- HOME EVENTS FETCH
+
+export const getHomeEvents = (filters = {}) => {
+  //fetch with filter object (can add all filter combinations, using just one fetch)
+
+  return async (dispatch) => {
+    dispatch({ type: GET_EVENTS_LOADING });
+
+    try {
+      const eventsUrl = "http://localhost:7001/events";
+      const token = localStorage.getItem("token");
+
+      const query = new URLSearchParams(filters).toString();
+
+      const response = await fetch(`${eventsUrl}?${query}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error("Error fetching events");
+      }
+
+      dispatch({
+        type: GET_HOME_EVENTS_SUCCESS,
+        payload: data.content,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_EVENTS_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
+
+// ---------- USERS PAST EVENTS FETCH
+
+export const getUserPastEvents = (userId) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_EVENTS_LOADING });
+
+    const eventsUrl = "http://localhost:7001/events";
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await fetch(
+        `${eventsUrl}?creatorId=${userId}&timeFilter=past`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const data = await response.json();
+
+      dispatch({
+        type: GET_USERS_PAST_EVENTS_SUCCESS,
+        payload: data.content,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_EVENTS_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
--- src/redux/reducers/EventReducer.js
+++ src/redux/reducers/EventReducer.js
@@ -1,7 +1,79 @@
-// const initialState = {
-//   created: [],
-//   joined: [],
-//   past: [],
-//   loading: false,
-//   error: null,
-// };
+import {
+  GET_EVENTS_LOADING,
+  GET_EVENTS_ERROR,
+  GET_HOME_EVENTS_SUCCESS,
+  GET_USERS_PAST_EVENTS_SUCCESS,
+  GET_USERS_FUTURE_EVENTS_SUCCESS,
+  GET_USERS_JOINED_EVENTS_SUCCESS,
+} from "../actions/EventActions";
+
+const initialState = {
+  homeEvents: [],
+  userEvents: {
+    pastEvents: [],
+    futureEvents: [],
+    joinedEvents: [],
+  },
+  loading: false,
+  error: null,
+};
+
+function EventReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_EVENTS_LOADING:
+      return {
+        ...state,
+        loading: true,
+        events: action.payload,
+      };
+
+    case GET_HOME_EVENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        homeEvents: action.payload,
+      };
+
+    case GET_USERS_PAST_EVENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        userEvents: {
+          ...state.userEvents,
+          pastEvents: action.payload,
+        },
+      };
+
+    case GET_USERS_FUTURE_EVENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        userEvents: {
+          ...state.userEvents,
+          futureEvents: action.payload,
+        },
+      };
+
+    case GET_USERS_JOINED_EVENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        userEvents: {
+          ...state.userEvents,
+          joinedEvents: action.payload,
+        },
+      };
+
+    case GET_EVENTS_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export default EventReducer;
